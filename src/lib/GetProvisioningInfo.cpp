b37fac8c06b26565fb140421d90173481fedde66
Terry Moore <tmm@mcci.com>
2016-11-03 22:23:42 -0400

    Clean up compiler warning, remove old provisioning info

--- src/lib/GetProvisioningInfo.cpp
+++ src/lib/GetProvisioningInfo.cpp
@@ -39,43 +39,6 @@ Revision history:
 |
 \****************************************************************************/
 
-#if 1
-
-#else
-
-static const Arduino_LoRaWAN::ProvisioningInfo
-skProvisioningInfo[] =
-    {
-        {  // 00-02-CC-01-00-00-00-06
-        Style:  Arduino_LoRaWAN::ProvisioningStyle::kOTAA,
-        AbpInfo: {},
-        OtaaInfo:
-                {
-                AppKey:  {0x29, 0xA6, 0xE3, 0xEF, 0x78, 0x4E, 0x46, 0x0D, 0xF8, 0xC8, 0xD5, 0xFD, 0x59, 0x41, 0x17, 0xF9},
-                DevEUI: { 0x06, 0x00, 0x00, 0x00, 0x01, 0xCC, 0x02, 0x00 },
-                AppEUI: { 0x52, 0x08, 0x00, 0xD0, 0x7E, 0xD5, 0xB3, 0x70 },
-                },
-        },
-
-        {  // 00-02-CC-01-00-00-00-0A
-        Style:  Arduino_LoRaWAN::ProvisioningStyle::kOTAA,
-	AbpInfo: {},
-        OtaaInfo:
-                {
-                AppKey:  {0x29, 0xA6, 0xE3, 0xEF, 0x78, 0x4E, 0x46, 0x0D, 0xF8, 0xC8, 0xD5, 0xFD, 0x59, 0x41, 0x17, 0xF9},
-                DevEUI: { 0x0A, 0x00, 0x00, 0x00, 0x01, 0xCC, 0x02, 0x00 },
-                AppEUI: { 0x52, 0x08, 0x00, 0xD0, 0x7E, 0xD5, 0xB3, 0x70 },
-                },
-        },
-    };
-
-const Arduino_LoRaWAN::ProvisioningTable
-gk_LoRaWAN_Keys =
-	{
-	pInfo: skProvisioningInfo,
-	nInfo: MCCIADK_LENOF(skProvisioningInfo)
-	};
-#endif
 
 /****************************************************************************\
 |
@@ -97,9 +60,6 @@ Catena4410::GetProvisioningInfo(
         {
         const Arduino_LoRaWAN::ProvisioningTable * const pTable =
                 &Catena4410::gk_LoRaWAN_Keys;
-
-        if (! pTable)
-                return NULL;
 
         const EUI64_buffer_t * const pSysEUI = this->GetSysEUI();
 
