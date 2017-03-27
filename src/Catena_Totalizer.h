4ab72054851687b903382472302cec694686aa82
Terry Moore <tmm@mcci.com>
2017-03-27 03:49:16 -0400

    Add totalizer for power pulses

---
+++ src/Catena_Totalizer.h
@@ -0,0 +1,87 @@
+/* Catena_Totalizer.h	Mon Mar 27 2017 00:18:49 tmm */
+
+/*
+
+Module:  Catena_Totalizer.h
+
+Function:
+	class McciCatena:cTotalizer.
+
+Version:
+	V0.5.0	Mon Mar 27 2017 00:18:49 tmm	Edit level 1
+
+Copyright notice:
+	This file copyright (C) 2017 by
+
+		MCCI Corporation
+		3520 Krums Corners Road
+		Ithaca, NY  14850
+
+	An unpublished work.  All rights reserved.
+	
+	This file is proprietary information, and may not be disclosed or
+	copied without the prior permission of MCCI Corporation.
+ 
+Author:
+	Terry Moore, MCCI Corporation	March 2017
+
+Revision history:
+   0.5.0  Mon Mar 27 2017 00:18:49  tmm
+	Module created.
+
+*/
+
+#ifndef _CATENA_TOTALIZER_H_		/* prevent multiple includes */
+#define _CATENA_TOTALIZER_H_
+
+#pragma once
+
+#include <cstdint>
+
+#ifndef _CATENA_POLLABLEINTERFACE_H_
+# include "Catena_PollableInterface.h"
+#endif
+
+namespace McciCatena {
+
+class cTotalizer : public cPollableObject
+	{
+public:
+	enum : uint8_t
+		{
+		kPinUndefined = 0xFFu,
+		};
+
+	cTotalizer(uint8_t pin = kPinUndefined) : m_pin(pin) {};
+
+	// setup, passing in a pin
+	bool begin(uint8_t pin = kPinUndefined);
+
+	// totalize
+	virtual void poll() override;
+
+	// get the running total
+	uint32_t getcurrent() const
+		{ return this->m_total; };
+
+private:
+        // total number of counts seen
+	uint32_t m_total = 0;
+        // time of last change (from millis())
+        uint32_t m_tEdge;
+        // last bit value observed.
+	bool m_last;
+        // last debounced bit value observed
+        bool m_lastStable;
+        // value has stabilized
+        bool m_fIsStable;
+        // counter is initialized
+	bool m_initialized = false;
+        // pin to monitor.
+	uint8_t m_pin;
+	};
+
+}; // namespace McciCatena
+
+/**** end of Catena_Totalizer.h ****/
+#endif /* _CATENA_TOTALIZER_H_ */
---
+++ src/lib/Catena_Totalizer.cpp
@@ -0,0 +1,106 @@
+/* Catena_Totalizer.cpp	Mon Mar 27 2017 00:27:22 tmm */
+
+/*
+
+Module:  Catena_Totalizer.cpp
+
+Function:
+	class McciCatena::Totalizer implemenation.
+
+Version:
+	V0.5.0	Mon Mar 27 2017 00:27:22 tmm	Edit level 1
+
+Copyright notice:
+	This file copyright (C) 2017 by
+
+		MCCI Corporation
+		3520 Krums Corners Road
+		Ithaca, NY  14850
+
+	An unpublished work.  All rights reserved.
+	
+	This file is proprietary information, and may not be disclosed or
+	copied without the prior permission of MCCI Corporation.
+ 
+Author:
+	Terry Moore, MCCI Corporation	March 2017
+
+Revision history:
+   0.5.0  Mon Mar 27 2017 00:27:22  tmm
+	Module created.
+
+*/
+
+#include "Catena_Totalizer.h"
+
+#include "CatenaBase.h"
+
+using namespace McciCatena;
+
+/****************************************************************************\
+|
+|	Method functions
+|
+\****************************************************************************/
+
+bool
+McciCatena::cTotalizer::begin(
+	uint8_t pin
+	)
+	{
+	if (this->m_pin == kPinUndefined &&
+	    pin == kPinUndefined)
+		{
+		return false;
+		}
+
+	if (this->m_initialized)
+		return true;
+
+	if (this->m_pin == kPinUndefined)
+		this->m_pin = pin;
+
+	// set up the input
+	pinMode(this->m_pin, INPUT);
+	this->m_last = digitalRead(this->m_pin);
+	this->m_lastStable = this->m_last;
+	this->m_initialized = true;
+	this->m_fIsStable = true;
+
+	// register with the polling engine
+	CatenaBase::pCatenaBase->registerObject(this);
+
+	return true;
+	}
+
+// public: virtual override
+void 
+McciCatena::cTotalizer::poll()
+	{
+	bool fCurrent = digitalRead(this->m_pin);
+	uint32_t now = millis();
+
+	if (fCurrent != this->m_last)
+		{
+		this->m_fIsStable = false;
+		this->m_tEdge = now;
+		this->m_last = fCurrent;
+		}
+	// current == last
+	else if (! this->m_fIsStable)
+		{
+		// 50 millisecond debounce?
+		if ((now - this->m_tEdge) > 50)
+			{
+			this->m_fIsStable = true;
+			if (fCurrent != this->m_lastStable)
+				{
+				// count on falling edges
+				if (! fCurrent)
+					++this->m_total;
+
+				this->m_lastStable = fCurrent;
+				}
+			}
+		}
+	}
