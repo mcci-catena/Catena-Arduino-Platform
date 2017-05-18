2319b724b40fec8b0852f2213b69923631b5a838
Terry Moore <tmm@mcci.com>
2017-05-18 18:08:55 -0400

    Clean compile; but still have FRAM init ordering probs

---
+++ src/lib/Catena4450_GetPlatformForID.cpp
@@ -0,0 +1,209 @@
+/* Catena4450_GetPlatformForID.cpp	Wed Apr 12 2017 13:26:14 tmm */
+
+/*
+
+Module:  Catena4450_GetPlatformForID.cpp
+
+Function:
+	Catena4450::GetPlatformForID()
+
+Version:
+	V0.5.0	Wed Apr 12 2017 13:26:14 tmm	Edit level 1
+
+Copyright notice:
+	This file copyright (C) 2017 by
+
+		MCCI Corporation
+		3520 Krums Corners Road
+		Ithaca, NY  14850
+
+	An unpublished work.  All rights reserved.
+	
+	This file is proprietary information, and may not be disclosed or
+	copied without the prior permission of MCCI Corporation.
+ 
+Author:
+	Terry Moore, MCCI Corporation	April 2017
+
+Revision history:
+   0.5.0  Wed Apr 12 2017 13:26:14  tmm
+	Module created.
+
+*/
+
+#include "Catena4450.h"
+
+#include "Catena_Log.h"
+#include "Catena_Platforms.h"
+
+using namespace McciCatena;
+
+const CATENA_PLATFORM (* const Catena4450::vPlatforms[]) =
+	{
+	// entry 0 is the default
+	&gkPlatformCatena4450,
+	&gkPlatformCatena4450_m101,
+	&gkPlatformCatena4450_m102,
+	};
+
+const size_t Catena4450::nvPlatforms = sizeof(Catena4450::vPlatforms) / sizeof(Catena4450::vPlatforms[0]);
+
+/*
+
+Name:	Catena4450::GetPlatformForID()
+
+Function:
+	Get a platform given a CPU ID and FRAM.
+
+Definition:
+	public: virtual 
+		const CATENA_PLATFORM * 
+			Catena4450::GetPlatformForID(
+				const UniqueID_buffer_t *pIdBuffer,
+				EUI64_buffer_t *pSysEui,
+				uint32_t *pOperatingFlags
+				) override;
+
+Description:
+	This override for GetPlatformForID() looks at the FRAM, and
+	picks any information availble there. If none is available, it falls
+	back to the generic SAM21D method, which scans by CPU ID.
+
+Returns:
+	Pointer to platform, or NULL. *pSysEUI is alway set to some value,
+	possibly zero if the routine fails. If non-NULL, pOperatingFlags is
+	set to the operating flags, possibly zero if the routine fails.
+
+*/
+
+/* public virtual override */
+const CATENA_PLATFORM * 
+Catena4450::GetPlatformForID(
+	const UniqueID_buffer_t *pIdBuffer,
+	EUI64_buffer_t *pSysEui,
+	uint32_t *pOperatingFlags
+	)
+	{
+	/* we ignore the CPUID unless we can't get a GUID */
+	MCCIADK_GUID_WIRE PlatformGuid;
+
+        gLog.printf(gLog.kAlways, "Catena4450::GetPlatformForID entered\n");
+
+	// set up the SysEUI
+	if (!this->m_Fram.getField(
+		cFramStorage::StandardKeys::kSysEUI,
+		*pSysEui
+		))
+		{
+                gLog.printf(cLog::DebugFlags(gLog.kInfo | gLog.kError), "Didn't find SysEUI entry; zero local copy\n");
+		memset(pSysEui->b, 0, sizeof(pSysEui->b));
+		}
+
+	if (!this->m_Fram.getField(cFramStorage::StandardKeys::kPlatformGuid,
+			PlatformGuid
+			))
+		{
+		const CATENA_PLATFORM *pRomPlatform;
+		uint32_t OperatingFlags;
+
+                gLog.printf(cLog::DebugFlags(gLog.kInfo | gLog.kError), "Didn't find FRAM entry; search well-known\n");
+
+		pRomPlatform = this->Super::GetPlatformForID(
+			pIdBuffer, pSysEui, &OperatingFlags
+			);
+
+		if (pRomPlatform != nullptr)
+			{
+			this->savePlatform(*pRomPlatform, pSysEui, &OperatingFlags);
+			}
+
+		if (pOperatingFlags)
+			*pOperatingFlags = OperatingFlags;
+
+		return pRomPlatform;
+		}
+
+	// set up the operating flags
+	uint32_t SavedFlags;
+	bool fHaveFlags = this->m_Fram.getField(
+				cFramStorage::StandardKeys::kOperatingFlags,
+				SavedFlags
+				);
+	if (! fHaveFlags)
+		{
+		SavedFlags = 0;
+		}
+
+	// search for a matching GUID		
+        const CATENA_PLATFORM *pPlatform;
+
+	for (size_t i = 0; i < nvPlatforms; ++i)
+		{
+                pPlatform = vPlatforms[i];
+
+		if (memcmp(&vPlatforms[i]->Guid, &PlatformGuid,
+			   sizeof(PlatformGuid)) == 0)
+			{
+			uint32_t Flags;
+
+			Flags = pPlatform->OperatingFlags;
+
+			// try to get the operating flags. If not found,
+			// use the value from *pPlatform as the default, and
+			// intialize the FRAM.
+			if (! fHaveFlags)
+				{
+                                this->m_Fram.saveField(
+					cFramStorage::StandardKeys::kOperatingFlags,
+					Flags
+					);
+				SavedFlags = Flags;
+				}
+
+			// give the operating flags back to the client.
+			if (pOperatingFlags != nullptr)
+				*pOperatingFlags = SavedFlags;
+
+			return pPlatform;
+			}
+		}
+
+	/* 
+	|| no platforms matched... set things up in a default-ish way.
+	*/
+	pPlatform = vPlatforms[0];
+	if (! fHaveFlags)
+		SavedFlags = pPlatform->OperatingFlags;
+
+	this->savePlatform(*pPlatform, nullptr, &SavedFlags);
+
+	return nullptr;
+	}
+
+void
+Catena4450::savePlatform(
+	const CATENA_PLATFORM &Platform,
+	const EUI64_buffer_t *pSysEUI,
+	const uint32_t *pOperatingFlags
+	)
+	{
+        this->m_Fram.saveField(
+		cFramStorage::StandardKeys::kPlatformGuid,
+		Platform.Guid
+		);
+
+	if (pSysEUI != nullptr)
+		{
+                this->m_Fram.saveField(
+			cFramStorage::StandardKeys::kSysEUI,
+			*pSysEUI
+			);
+		}
+	if (pOperatingFlags != nullptr)
+		{
+                this->m_Fram.saveField(
+			cFramStorage::StandardKeys::kOperatingFlags,
+			*pOperatingFlags
+			);
+		}
+	}
