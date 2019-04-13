cae26fcf7b78254ad8287e0dfcbffea6f7b9432b
Terry Moore <tmm@mcci.com>
2019-04-13 19:39:22 -0400

    Fix #140: add example sketch for user commands

---
+++ src/Catena_CommandStream_vmicro_fixup.h
@@ -0,0 +1,24 @@
+/*
+
+Module:	Catena_CommandStream_vmicro_fixup.h
+
+Function:
+        Define some symbols needed by Visual Micro .ino processing.
+
+Copyright:
+        See accompanying LICENSE file.
+
+Author:
+        Terry Moore, MCCI Corporation	April 2019
+
+*/
+
+#pragma once
+
+#ifndef _CATENA_COMMANDSTREAM_VMICRO_FIXUP_H_
+#define _CATENA_COMMANDSTREAM_VMICRO_FIXUP_H_
+
+typedef McciCatena::cCommandStream::CommandStatus CommandStatus;
+typedef McciCatena::cCommandStream cCommandStream;
+
+#endif /* _CATENA_COMMANDSTREAM_VMICRO_FIXUP_H_ */
