2b295274515b44e4248c94e79946b02863fe956f
Terry Moore <tmm@mcci.com>
2021-11-22 05:16:24 +0100

    Tabs to spaces, indent 4

--- src/lib/CatenaBase.cpp
+++ src/lib/CatenaBase.cpp
@@ -3,13 +3,13 @@
 Module:  CatenaBase.cpp
 
 Function:
-	Implemenation of class CatenaBase;
+    Implemenation of class CatenaBase;
 
 Copyright notice:
-        See accompanying LICENSE file.
+    See accompanying LICENSE file.
 
 Author:
-	Terry Moore, MCCI Corporation	March 2017
+    Terry Moore, MCCI Corporation	March 2017
 
 */
 
@@ -24,62 +24,62 @@ CatenaBase *CatenaBase::pCatenaBase;
 
 /* the constructor */
 CatenaBase::CatenaBase(Version_t v)
-	{
-	this->m_appVersion = v;
-	pCatenaBase = this;
-	}
+    {
+    this->m_appVersion = v;
+    pCatenaBase = this;
+    }
 
 /* begin operating */
 bool
 CatenaBase::begin(void)
-	{
-	UniqueID_buffer_t CpuID;
+    {
+    UniqueID_buffer_t CpuID;
 
-	// use default debug flags
-	gLog.begin();
+    // use default debug flags
+    gLog.begin();
 
-	this->m_PollingEngine.begin();
+    this->m_PollingEngine.begin();
 
-	/* get the CPU ID */
-	this->GetUniqueID(&CpuID);
-	this->m_pPlatform = this->GetPlatformForID(
-				&CpuID,
-				&this->m_SysEUI,
-				&this->m_OperatingFlags
-				);
+    /* get the CPU ID */
+    this->GetUniqueID(&CpuID);
+    this->m_pPlatform = this->GetPlatformForID(
+                                &CpuID,
+                                &this->m_SysEUI,
+                                &this->m_OperatingFlags
+                                );
 
-	// set up the command line collector
-	this->m_Collector.begin(&Serial, &this->m_SerialReady);
-	this->registerObject(&this->m_Collector);
+    // set up the command line collector
+    this->m_Collector.begin(&Serial, &this->m_SerialReady);
+    this->registerObject(&this->m_Collector);
 
-	// set up the command line processor
-	this->m_CommandStream.begin(&this->m_Collector, this);
+    // set up the command line processor
+    this->m_CommandStream.begin(&this->m_Collector, this);
 
-	return true;
-	}
+    return true;
+    }
 
 /* register a pollable object with the polling engine */
 void
 CatenaBase::registerObject(
-	McciCatena::cPollableObject *pObject
-	)
-	{
-	this->m_PollingEngine.registerObject(pObject);
-	}
+    McciCatena::cPollableObject *pObject
+    )
+    {
+    this->m_PollingEngine.registerObject(pObject);
+    }
 
 /* poll all the registered objects */
 void
 CatenaBase::poll()
-	{
-	this->m_PollingEngine.poll();
-	}
+    {
+    this->m_PollingEngine.poll();
+    }
 
 /* add a number of commands to the command table */
 void
 CatenaBase::addCommands(
-	cCommandStream::cDispatch &dispatch,
-	void *pContext
-	)
-	{
-	this->m_CommandStream.registerCommands(&dispatch, pContext);
-	}
+    cCommandStream::cDispatch &dispatch,
+    void *pContext
+    )
+    {
+    this->m_CommandStream.registerCommands(&dispatch, pContext);
+    }
